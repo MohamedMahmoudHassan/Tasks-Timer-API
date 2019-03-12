f68518ab2eb0ec0d3b658fabeef26cb3b1388fd5
MohamedMahmoudHassan <mohamedmahmoudalihassan@gmail.com>
2019-03-12 13:14:28 +0200

    analyseTime function fixed

--- models/functions.js
+++ models/functions.js
@@ -5,11 +5,11 @@ function add(acc, item) {
 function analyseTime(time) {
     const ms = time % 100;
     time = Math.floor(time / 100);
-    const sec = time %60;
+    const sec = time % 60;
     time = Math.floor(time / 60);
-    const min = time %60;
+    const min = time % 60;
     time = Math.floor(time / 60);
-    const hour = time %60;
+    const hour = time % 24;
     time = Math.floor(time / 24);
     const day = time;
     return { day, hour, min, sec, ms };
